976876984f1ac6b074bb1ab549ec63382e9a637b
Santi Espinosa <saigesp@gmail.com>
2019-08-10 12:33:10 +0200

    add styles and title font

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import './assets/styles/reset.scss';
+import './assets/styles/flexbox.scss';
+import './assets/styles/fonts.scss';
+import './assets/styles/main.scss';
+
 Vue.config.productionTip = false
 
 new Vue({
