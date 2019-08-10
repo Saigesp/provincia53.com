39bd02f533907176ba8b5019c9aed2de9beed067
Santi Espinosa <saigesp@gmail.com>
2019-08-10 12:40:21 +0200

    rename Soon component to AviatComponent

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import AviatComponent from '@/components/AviatComponent'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'AviatComponent',
+      component: AviatComponent
     }
   ]
 })
