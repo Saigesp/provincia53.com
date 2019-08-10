d5f58134ed487068efcbd38808d1dc040ecdfa5b
Saigesp <saigesp@gmail.com>
2019-08-10 03:54:24 +0200

    vue webpack init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>Provincia53</h1>
+    <p>Próximamente</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<style>
+.hello {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  font-weight: normal;
+}
+</style>
