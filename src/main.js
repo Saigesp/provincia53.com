32af742dddc4480f0b0c47350af420c65bfa0af3
Santi Espinosa <saigesp@gmail.com>
2019-09-05 09:33:46 +0200

    change forcegraph to custom vue dataviz

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import VueMouseParallax from 'vue-mouse-parallax'
+Vue.use(VueMouseParallax)
+
 import './assets/styles/reset.scss';
 import './assets/styles/flexbox.scss';
 import './assets/styles/fonts.scss';
